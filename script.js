13742a196efc6605a275581727d1012f31b41686
Niyaz <niyazelimale@gmail.com>
2025-06-27 20:22:21 +0530

    test: Add US stocks to debug API

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // --- CONFIGURATION ---
 // IMPORTANT: We will replace '__API_KEY__' with our actual key during the CI/CD process.
 const API_KEY = '__API_KEY__'; 
-const STOCKS_TO_TRACK = ['NVDA', 'INTC', 'QCOM', 'AAPL', 'MSFT', 'RELIANCE.NS', 'TCS.NS', 'HDFCBANK.NS', 'INFY.NS', 'ICICIBANK.NS', 'SBIN.NS'];
+const STOCKS_TO_TRACK = ['NVDA', 'INTC', 'QCOM', 'AAPL', 'MSFT', 'GOOGL', 'AMZN', 'META', 'TSLA', 'AMD'] 
 
 // --- DOM ELEMENTS ---
 const stockContainer = document.getElementById('stock-container');
